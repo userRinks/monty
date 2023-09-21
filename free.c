48dff5118ed8d5ad533cc41b52e91cd3bad57c96
MistaRinks <rinkanyakevin@gmail.com>
2023-09-21 08:54:12 +0300

    Free stack

---
+++ free.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * free_stack - Free the list
+ * @current: pointer to current head of list
+ *
+ * Description: Checks list and frees elements
+ * Return: (_void_)
+ */
+
+void free_stack(stack_t *current_node)
+{
+	stack_t *next_node;
+
+	/* If list is free, stop executing */
+	if (!current_node)
+		return;
+
+	/* Move step by step freeing elements */
+	next_node = current_node;
+
+	while (next_node)
+	{
+		current_node = next_node;
+		next_node = current_node->next;
+		free(current_node);
+	}
+}
