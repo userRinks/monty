3943c91f366ca56d5c854573e8a82757503f6154
MistaRinks <rinkanyakevin@gmail.com>
2023-09-22 02:12:04 +0300

    [Restructured:] Open, validate, process and free input

--- handle_input.c
+++ handle_input.c
@@ -36,46 +36,77 @@ void process_file(FILE *my_input)
 		line_number++;
 		instruction = parse_line(line);
 
-		if (!(instruction->opcode) || instruction->opcode[0] == '#')
-		{
-			/* Skip comments and empty lines */
-			free(instruction);
-			if (line)
-				free(line);
-			line = NULL;
+		if (skip_line(instruction, &line))
 			continue;
-		}
 
-		if (instruction->f)
+		if (execute_instruction(instruction, &top, line_number))
 		{
-			/* Execute the instruction function */
-			instruction->f(&top, line_number);
-		}
-		else
-		{
-			/* Handle unknown instructions */
 			fprintf(stdout, "L%d: unknown instruction %s\n",
 					line_number, instruction->opcode);
 			free(instruction);
-
-			/* Clean up stack */
-			if (line)
-				free(line);
 			free_stack(top);
 			fclose(my_input);
+			free(line);
 			exit(EXIT_FAILURE);
 		}
 
-		/* Clean up parsed instruction and line */
 		free(instruction);
-		if (line)
-			free(line);
-		line = NULL;
 	}
 
-    /* Clean up resources and close the file */
-	if (line)
-		free(line);
+	free(line);
+	free_stack(top);
+}
+
+/**
+ * skip_line - Skip comments and empty lines
+ * @instruction: Parsed instruction
+ * @line: Current line
+ * Return: 1 if the line should be skipped, 0 otherwise
+ */
+int skip_line(instruction_t *instruction, char **line)
+{
+	if (!instruction->opcode || instruction->opcode[0] == '#')
+	{
+		/* free(instruction); */
+		free(*line);
+		*line = NULL;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * execute_instruction - Execute the Monty instruction
+ * @instruction: Parsed instruction
+ * @top: Pointer to the top of the stack
+ * @line_number: Current line number
+ * Return: 1 if an unknown instruction, 0 otherwise
+ */
+int execute_instruction(instruction_t *instruction,
+		stack_t **top, unsigned int line_number)
+{
+	if (instruction->f)
+	{
+		instruction->f(top, line_number);
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * handle_unknown_instruction - Handle unknown instructions
+ * @instruction: Parsed instruction
+ * @line_number: Current line number
+ * @my_input: File pointer
+ * @top: Pointer to the top of the stack
+ */
+void handle_unknown_instruction(instruction_t *instruction,
+		unsigned int line_number, FILE *my_input, stack_t *top)
+{
+	fprintf(stdout, "L%d: unknown instruction %s\n",
+			line_number, instruction->opcode);
+	free(instruction);
 	free_stack(top);
 	fclose(my_input);
+	exit(EXIT_FAILURE);
 }
